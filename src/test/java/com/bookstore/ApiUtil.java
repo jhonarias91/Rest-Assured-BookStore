d4dc63a501b2f248a875fdd6ed9883441e82410a
jhonarias91 <38085369+jhonarias91@users.noreply.github.com>
2024-10-22 11:55:18 -0500

    Feature/test login (#2)
    Updated package name, created with wrong name at beginning

---
+++ src/test/java/com/bookstore/ApiUtil.java
@@ -0,0 +1,20 @@
+package com.bookstore;
+
+import com.bookstore.model.User;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiUtil {
+
+    private static final String BASE_RUL = "https://demoqa.com/";
+    public static Response createUser(final String userName, final String password) {
+        return given()
+                .contentType(ContentType.JSON)
+                .body(new User(userName, password))
+                .when()
+                .post(BASE_RUL+"Account/v1/User");
+    }
+
+}
