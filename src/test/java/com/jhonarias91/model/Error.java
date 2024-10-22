b75d070a59c8f3c0be3adeb7ca7a86d6eb7f6819
jhon.arias <jhonarias91@gmail.com>
2024-10-22 11:50:34 -0500

    Reto 1. Created ApiUtil parent class to create and get user created

---
+++ src/test/java/com/jhonarias91/model/Error.java
@@ -0,0 +1,28 @@
+package com.jhonarias91.model;
+
+import java.util.List;
+
+public class Error {
+
+
+    private String code;
+    private String message;
+    public Error() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
